19a7a63d3da168494ae1946c9ea88c862a0d931f
Blazej Bielski <bbielski@lyck.pl>
2022-11-14 19:40:09 +0100

    script for data generator

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,6 @@
+import json
 import random
 import datetime
-from pprint import pprint
 
 
 class DataGenerator:
@@ -20,7 +20,7 @@ class DataGenerator:
         for idx in range(amount):
             event = {
                 'idx': idx,
-                'start_date': self.beginning_date + datetime.timedelta(hours=random.randint(1, 5000)),
+                'start_date': f'{(self.beginning_date + datetime.timedelta(hours=random.randint(1, 5000))):%Y/%m/%d %H:%M}',
                 'duration': random.randint(*self.duration_range),
                 'title': random.choice(self.titles),
                 'description': random.choice(self.descriptions),
@@ -35,18 +35,49 @@ class DataGenerator:
 
             events.append(event)
 
-        pprint(events)
+        return events
+
+    @staticmethod
+    def save_data(data, path):
+        with open(path, 'w') as file:
+            json.dump(data, file)
 
 
-d = DataGenerator(
+event_data = DataGenerator(
     datetime.date.today() + datetime.timedelta(days=10),
     (15, 180),
     ['lunch', 'ceo meeting', 'lecture', 'seminar', 'sport event'],
     ['nice meeting', 'troublesome meeting', 'god, I hate that', 'kill me', 'emergency meeting'],
     ['Pawel', 'Marek', 'Darek', 'Jarek', 'Andrzej', 'Ryszard'],
     False,
-    False
+    False,
 )
 
+events = event_data.generate_data(150)
+event_data.save_data(events, 'event_data.json' )
 
-d.generate_data(50)
+remind_data = DataGenerator(
+    datetime.date.today() + datetime.timedelta(days=10),
+    (15, 180),
+    ['lunch', 'ceo meeting', 'lecture', 'seminar', 'sport event'],
+    ['nice meeting', 'troublesome meeting', 'god, I hate that', 'kill me', 'emergency meeting'],
+    ['Pawel', 'Marek', 'Darek', 'Jarek', 'Andrzej', 'Ryszard'],
+    True,
+    False,
+)
+
+reminders = remind_data.generate_data(50)
+remind_data.save_data(reminders, 'remind_data.json')
+
+workshop_data = DataGenerator(
+    datetime.date.today() + datetime.timedelta(days=10),
+    (15, 180),
+    ['lunch', 'ceo meeting', 'lecture', 'seminar', 'sport event'],
+    ['nice meeting', 'troublesome meeting', 'god, I hate that', 'kill me', 'emergency meeting'],
+    ['Pawel', 'Marek', 'Darek', 'Jarek', 'Andrzej', 'Ryszard'],
+    False,
+    True,
+)
+
+workshops = workshop_data.generate_data(50)
+workshop_data.save_data(workshops, 'workshop_data.json')
