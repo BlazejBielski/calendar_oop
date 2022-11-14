4c7ee637a5fbe1eee2f8d656f115d070de2455d9
Blazej Bielski <bbielski@lyck.pl>
2022-11-14 19:49:15 +0100

    add load data from files

--- data_generator.py
+++ data_generator.py
@@ -42,6 +42,11 @@ class DataGenerator:
         with open(path, 'w') as file:
             json.dump(data, file)
 
+    @staticmethod
+    def load_data(path):
+        with open(path, 'r') as file:
+            return json.load(path)
+
 
 event_data = DataGenerator(
     datetime.date.today() + datetime.timedelta(days=10),
@@ -53,10 +58,10 @@ event_data = DataGenerator(
     False,
 )
 
-events = event_data.generate_data(150)
-event_data.save_data(events, 'event_data.json' )
+event_datas = event_data.generate_data(150)
+event_data.save_data(event_datas, 'event_data.json')
 
-remind_data = DataGenerator(
+event_data = DataGenerator(
     datetime.date.today() + datetime.timedelta(days=10),
     (15, 180),
     ['lunch', 'ceo meeting', 'lecture', 'seminar', 'sport event'],
@@ -66,18 +71,18 @@ remind_data = DataGenerator(
     False,
 )
 
-reminders = remind_data.generate_data(50)
-remind_data.save_data(reminders, 'remind_data.json')
+# reminders = event_data.generate_data(50)
+# event_data.save_data(reminders, 'remind_data.json')
 
-workshop_data = DataGenerator(
-    datetime.date.today() + datetime.timedelta(days=10),
-    (15, 180),
-    ['lunch', 'ceo meeting', 'lecture', 'seminar', 'sport event'],
-    ['nice meeting', 'troublesome meeting', 'god, I hate that', 'kill me', 'emergency meeting'],
-    ['Pawel', 'Marek', 'Darek', 'Jarek', 'Andrzej', 'Ryszard'],
-    False,
-    True,
-)
-
-workshops = workshop_data.generate_data(50)
-workshop_data.save_data(workshops, 'workshop_data.json')
+# workshop_data = DataGenerator(
+#     datetime.date.today() + datetime.timedelta(days=10),
+#     (15, 180),
+#     ['lunch', 'ceo meeting', 'lecture', 'seminar', 'sport event'],
+#     ['nice meeting', 'troublesome meeting', 'god, I hate that', 'kill me', 'emergency meeting'],
+#     ['Pawel', 'Marek', 'Darek', 'Jarek', 'Andrzej', 'Ryszard'],
+#     False,
+#     True,
+# )
+#
+# workshops = workshop_data.generate_data(50)
+# workshop_data.save_data(workshops, 'workshop_data.json')
